d90ffdd6e37488cb56c1ce66e572c1c15c502e5d
iafs32 <iafs.0232@gmail.com>
2024-04-24 18:06:02 -0600

    Añade una imagen del fondo

--- main.py
+++ main.py
@@ -1,44 +1,32 @@
-import sys
-
 import pygame
 
-class Game:
-    def __init__(self) -> None:
-        
-        pygame.init()
+pygame.init()
 
-        pygame.display.set_caption('stickman fight')
-        self.screen = pygame.display.set_mode((640, 480))
+# Crear la ventana
+SCREEN_WIDTH = 1000
+SCREEN_HEIGT = 600
 
-        self.clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
+pygame.display.set_caption('Stickman Fight')
+
+# Cargar imágen del fondo
+bg_image = pygame.image.load('./assets/images/background/fondo.png').convert_alpha()
+
+# Función para mostrar el fondo
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGT))
+    screen.blit(scaled_bg, (0,0))
+
+# Ejecución del juego
+run = True
+while run:
+    # Dibujar el fondo
+    draw_bg()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
         
-        self.img = pygame.image.load('./data/character/mono.png')
-        
-        self.img_pos = [160, 260]
-        self.movement = [False, False]
-    def run(self):
-        while True:
-            self.screen.fill((14, 219, 248))
-            self.img_pos[0] += (self.movement[0] - self.movement[1]) * 5
-            self.screen.blit(self.img, self.img_pos)
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.movement[0] = True
-                    if event.key == pygame.K_LEFT:
-                        self.movement[1] = True
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.movement[0] = False
-                    if event.key == pygame.K_LEFT:
-                        self.movement[1] = False
-                    
-            
-            pygame.display.update()
-            self.clock.tick(60)
-            
-Game().run()
+    pygame.display.update()
+# Quitar pygame
+pygame.quit()
